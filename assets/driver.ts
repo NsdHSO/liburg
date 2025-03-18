b774040245a836bc8a3965af5be53867bd3484c2
NsdHSO <37635083+NsdHSO@users.noreply.github.com>
2025-03-18 13:11:44 +0000

    Deploying to gh-pages from @ NsdHSO/liburg@ea101163284bdb553ba7091d01653bc35e2614f4 🚀

---
+++ assets/driver.ts
@@ -0,0 +1,54 @@
+export function drivers(): any {
+  return [
+    {
+      "id": 1,
+      "first_name": "WatTT",
+      "last_name": "Thunnercliff",
+      "email": "wthunnercliff0@tripadvisor.com",
+      "gender": "Male",
+      "license": "\"",
+      "revenu": 56,
+      "accidents": true
+    },
+    {
+      "id": 2,
+      "first_name": "Emmett",
+      "last_name": "Bette",
+      "email": "ebette1@exblog.jp",
+      "gender": "Bigender",
+      "license": "1",
+      "revenu": 2,
+      "accidents": true
+    },
+    {
+      "id": 3,
+      "first_name": "Mason",
+      "last_name": "Kemwal",
+      "email": "mkemwal2@creativecommons.org",
+      "gender": "Genderfluid",
+      "license": "̗̺͖̹̯͓Ṯ̤͍̥͇͈h̲́e͏͓̼̗̙̼̣͔ ͇̜̱̠͓͍ͅN͕͠e̗̱z̘̝̜̺͙p̤̺̹͍̯͚e̠̻̠͜r̨̤͍̺̖͔̖̖d̠̟̭̬̝͟i̦͖̩͓͔̤a̠̗̬͉̙n͚͜ ̻̞̰͚ͅh̵͉i̳̞v̢͇ḙ͎͟-҉̭̩̼͔m̤̭̫i͕͇̝̦n̗͙ḍ̟ ̯̲͕͞ǫ̟̯̰̲͙̻̝f ̪̰̰̗̖̭̘͘c̦͍̲̞͍̩̙ḥ͚a̮͎̟̙͜ơ̩̹͎s̤.̝̝ ҉Z̡̖̜͖̰̣͉̜a͖̰͙̬͡l̲̫̳͍̩g̡̟̼̱͚̞̬ͅo̗͜.̟",
+      "revenu": 50,
+      "accidents": false
+    },
+    {
+      "id": 4,
+      "first_name": "Jerome",
+      "last_name": "Merrington",
+      "email": "jmerrington3@symantec.com",
+      "gender": "Male",
+      "license": "-1",
+      "revenu": 21,
+      "accidents": false
+    },
+    {
+      "id": 5,
+      "first_name": "Elna",
+      "last_name": "Arnao",
+      "email": "earnao4@harvard.edu",
+      "gender": "Female",
+      "license": "-1.00",
+      "revenu": 56,
+      "accidents": false
+    }
+  ]
+}
