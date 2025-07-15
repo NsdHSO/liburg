cd47010ee9d3e7fd16e835a015ff46a4ba6b98f7
NsdHSO <nechiforelsamuel@yahoo.com>
2025-07-15 11:13:44 +0300

    feat: 🎸 tried to publish automatically

--- libs/ngx-liburg/src/lib/table/table/table.component.spec.ts
+++ libs/ngx-liburg/src/lib/table/table/table.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableComponent } from './table.component';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTableModule } from '@angular/material/table';
 
 describe('TableComponent', () => {
   let component: TableComponent<any>;
@@ -18,30 +18,10 @@ describe('TableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
-    component.dataSource = [
-      { model: {} as any } as any,
-      { model: {} as any } as any,
-    ];
-    component.zebraColor = true;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should zebra be in class list ', () => {
-    const elementRow = fixture.nativeElement.querySelector(
-      '[ data-test="mat-row-def"]'
-    );
-    expect(elementRow.classList).toContain('zebra');
-  });
-  it('should not zebra be in class list ', () => {
-    const elementRow = fixture.nativeElement.querySelector(
-      '[ data-test="mat-row-def"]'
-    );
-    component.zebraColor = false;
-    fixture.detectChanges();
-    expect(elementRow.classList).not.toContain('zebra');
   });
 });
